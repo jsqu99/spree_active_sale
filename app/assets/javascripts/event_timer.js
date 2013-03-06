ae0206757982f768e14147479b07865f3fd17565
Jeff Squires <jeff.squires@gmail.com>
2013-03-06 17:21:43 -0500

    removes the dom element after it expires

--- app/assets/javascripts/event_timer.js
+++ app/assets/javascripts/event_timer.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
   $("[data-timer]").each(function() {
     var cTime = $(this).attr('data-timer');
     cTime = Date.parse(cTime);
-    $(this).countdown({ 
-      until: new Date(cTime), 
-      compact: true, 
-      layout: '{dn} DAYS {hnn}{sep}{mnn}{sep}{snn}'
+    $(this).countdown({
+      until: new Date(cTime),
+      compact: true,
+      layout: '{d<}{dn} DAY(S) {d>}{hnn}{sep}{mnn}{sep}{snn}',
+      onExpiry: removeActiveSaleEventFromDom
     });
   });
 });
+
+function removeActiveSaleEventFromDom() {
+  $(this).closest('li').remove();
+}
