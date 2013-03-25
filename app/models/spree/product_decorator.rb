78b9ba6727603f86fcd8e94ca979b0c2d7961cca
Jeff Squires <jeff.squires@gmail.com>
2013-03-25 15:41:00 -0400

    shows child taxons as well

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,10 +2,27 @@ Spree::Product.class_eval do
   has_many :active_sale_events, :as => :eventable
 
   def find_live_taxons
-    Spree::ActiveSaleEvent.live_active.where(:eventable_type => "Spree::Taxon", :eventable_id => self.taxons.map(&:id))
+    Spree::ActiveSaleEvent.live_active.where(:eventable_type => "Spree::Taxon", :eventable_id => taxons_plus_children(self.taxons))
   end
 
   def live?
     !self.active_sale_events.detect{ |event| event.live_and_active? }.nil? || !self.find_live_taxons.blank?
   end
+
+  private
+    def taxons_plus_children(taxons)
+      ids = taxons.inject([]) { |ids, taxon|
+        puts "taxon: #{taxon}"
+        puts "ids: #{ids}"
+  
+        ids << [taxon.id]
+
+        taxons = taxon.children
+
+        if taxons.present?
+          ids << taxons_plus_children(taxons)
+        end
+        ids.flatten
+      }
+    end
 end
